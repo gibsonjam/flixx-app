6968afe4d2ecaec11c7a93f22e89e06cdaa1fdc6
Jamison Gibson <jamison.a.gibson@gmail.com>
2025-02-21 18:11:20 -0600

    added display tv show functionality

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ const global = {
   currentPage: window.location.pathname,
 };
 
+// Display 20 most popular movies
 async function displayPopularMovies() {
   const { results } = await fetchApiData('movie/popular');
   console.log(results);
@@ -29,17 +30,59 @@ async function displayPopularMovies() {
   });
 }
 
+// Display 20 most high rated tv shows
+async function displayRatedShows() {
+  const { results } = await fetchApiData('tv/top_rated');
+  console.log(results);
+
+  results.forEach(show => {
+    const div = document.createElement('div');
+    div.classList.add('card');
+    div.innerHTML = `
+      <a href="tv-details.html?id=${show.id}">
+        ${
+          show.poster_path
+            ? `<img src="https://image.tmdb.org/t/p/w500/${show.poster_path}" class="card-img-top" alt="${show.title}" />`
+            : `<img src="images/no-image.jpg" class="card-img-top" alt="${show.name}" />`
+        }
+      </a>
+      <div class="card-body">
+        <h5 class="card-title">${show.name}</h5>
+        <p class="card-text">
+          <small class="text-muted">Air Date: ${show.first_air_date}</small>
+        </p>
+      </div>
+    </div>;`;
+
+    document.getElementById('popular-shows').appendChild(div);
+  });
+
+  console.log(results);
+}
+
 // Fetch data from TMDB API
 async function fetchApiData(endpoint) {
   const API_KEY = '083d5692de280873c184435311901117';
   const apiUrl = 'https://api.themoviedb.org/3/';
+
+  showSpinner();
 
   const response = await fetch(
     `${apiUrl}${endpoint}?api_key=${API_KEY}&language=en-US`
   );
   const data = await response.json();
 
+  hideSpinner();
+
   return data;
+}
+
+function showSpinner() {
+  document.querySelector('.spinner').classList.add('show');
+}
+
+function hideSpinner() {
+  document.querySelector('.spinner').classList.remove('show');
 }
 
 // Highlight active link
@@ -61,7 +104,7 @@ function init() {
       displayPopularMovies();
       break;
     case '/shows.html':
-      console.log('shows');
+      displayRatedShows();
       break;
     case '/movie-details.html':
       console.log('movie details');
