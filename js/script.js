b2d7a85c967c8758dad5371b865f37026b7a09c5
Jamison Gibson <jamison.a.gibson@gmail.com>
2025-02-21 19:32:35 -0600

    add movie page functionality

--- js/script.js
+++ js/script.js
@@ -5,7 +5,6 @@ const global = {
 // Display 20 most popular movies
 async function displayPopularMovies() {
   const { results } = await fetchApiData('movie/popular');
-  console.log(results);
 
   results.forEach(movie => {
     const div = document.createElement('div');
@@ -33,7 +32,6 @@ async function displayPopularMovies() {
 // Display 20 most high rated tv shows
 async function displayRatedShows() {
   const { results } = await fetchApiData('tv/top_rated');
-  console.log(results);
 
   results.forEach(show => {
     const div = document.createElement('div');
@@ -56,8 +54,70 @@ async function displayRatedShows() {
 
     document.getElementById('popular-shows').appendChild(div);
   });
+}
 
-  console.log(results);
+// Display movie details
+async function displayMovieDetails() {
+  const movieId = window.location.search.split('=')[1];
+
+  const movie = await fetchApiData(`movie/${movieId}`);
+  console.log(movie);
+
+  const divTop = document.createElement('div');
+  const divBottom = document.createElement('div');
+
+  divTop.classList.add('details-top');
+  divBottom.classList.add('details-bottom');
+
+  divTop.innerHTML = `
+          <div>
+          ${
+            movie.poster_path
+              ? `<img src="https://image.tmdb.org/t/p/w500/${movie.poster_path}" class="card-img-top" alt="${movie.title}" />`
+              : `<img src="images/no-image.jpg" class="card-img-top" alt="${movie.title}" />`
+          }
+          </div>
+          <div>
+            <h2>${movie.title}</h2>
+            <p>
+              <i class="fas fa-star text-primary"></i>
+              ${movie.vote_average.toFixed(1)}
+            </p>
+            <p class="text-muted">Release Date: ${movie.release_date}</p>
+            <p>
+              ${movie.overview}
+            </p>
+            <h5>Genres</h5>
+            <ul class="list-group">
+              ${movie.genres.map(genre => `<li>${genre.name}</li>`).join('')}
+            </ul>
+            <a href="${
+              movie.homepage
+            }" target="_blank" class="btn">Visit Movie Homepage</a>
+          </div>
+        </div>`;
+
+  divBottom.innerHTML = `
+        <div class="details-bottom">
+          <h2>Movie Info</h2>
+          <ul>
+            <li><span class="text-secondary">Budget:</span> ${movie.budget}</li>
+            <li><span class="text-secondary">Revenue:</span> ${
+              movie.revenue
+            }</li>
+            <li><span class="text-secondary">Runtime:</span> ${
+              movie.runtime
+            }</li>
+            <li><span class="text-secondary">Status:</span> ${movie.status}</li>
+          </ul>
+          <h4>Production Companies</h4>
+          <div class="list-group">${movie.production_companies
+            .map(company => `<span>${company.name}</span>`)
+            .join(', ')}
+        </div>`;
+
+  document.querySelector('#movie-details').appendChild(divTop);
+  document.querySelector('#movie-details').appendChild(divBottom);
 }
 
 // Fetch data from TMDB API
@@ -107,7 +167,7 @@ function init() {
       displayRatedShows();
       break;
     case '/movie-details.html':
-      console.log('movie details');
+      displayMovieDetails();
       break;
     case '/tv-details.html':
       console.log('tv details');
