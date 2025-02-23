3fdc08b14abcd68faaef1aea35d0a565eba5ba6d
Jamison Gibson <jamison.a.gibson@gmail.com>
2025-02-22 20:06:34 -0600

    added movie and show details functionality

--- js/script.js
+++ js/script.js
@@ -61,7 +61,9 @@ async function displayMovieDetails() {
   const movieId = window.location.search.split('=')[1];
 
   const movie = await fetchApiData(`movie/${movieId}`);
-  console.log(movie);
+
+  // Overlay for background image
+  displayBackgroundImage('movie', movie.backdrop_path);
 
   const divTop = document.createElement('div');
   const divBottom = document.createElement('div');
@@ -120,6 +122,94 @@ async function displayMovieDetails() {
   document.querySelector('#movie-details').appendChild(divBottom);
 }
 
+// Display TV Details Page
+async function displayShowDetails() {
+  const showId = window.location.search.split('=')[1];
+
+  const show = await fetchApiData(`tv/${showId}`);
+
+  // Overlay for background image
+  displayBackgroundImage('show', show.backdrop_path);
+
+  const divTop = document.createElement('div');
+  const divBottom = document.createElement('div');
+
+  divTop.classList.add('details-top');
+  divBottom.classList.add('details-bottom');
+
+  divTop.innerHTML = `
+          <div>
+          ${
+            show.poster_path
+              ? `<img src="https://image.tmdb.org/t/p/w500/${show.poster_path}" class="card-img-top" alt="${show.title}" />`
+              : `<img src="images/no-image.jpg" class="card-img-top" alt="${show.title}" />`
+          }
+          </div>
+          <div>
+            <h2>${show.name}</h2>
+            <p>
+              <i class="fas fa-star text-primary"></i>
+              ${show.vote_average.toFixed(1)}
+            </p>
+            <p class="text-muted">Release Date: ${show.first_air_date}</p>
+            <p>
+              ${show.overview}
+            </p>
+            <h5>Genres</h5>
+            <ul class="list-group">
+              ${show.genres.map(genre => `<li>${genre.name}</li>`).join('')}
+            </ul>
+            <a href="${
+              show.homepage
+            }" target="_blank" class="btn">Visit show Homepage</a>
+          </div>
+        </div>`;
+
+  divBottom.innerHTML = `
+        <h2>Show Info</h2>
+          <ul>
+            <li><span class="text-secondary">Number Of Episodes:</span> ${
+              show.number_of_episodes
+            }</li>
+            <li>
+              <span class="text-secondary">Last Episode To Air:</span> ${
+                show.last_episode_to_air.name
+              }
+            </li>
+            <li><span class="text-secondary">Status:</span> ${show.status}</li>
+          </ul>
+          <h4>Production Companies</h4>
+          <div class="list-group">${show.production_companies
+            .map(company => `<span>${company.name}</span>`)
+            .join(', ')}</div>`;
+
+  document.querySelector('#show-details').appendChild(divTop);
+  document.querySelector('#show-details').appendChild(divBottom);
+}
+
+// Display Backdrop On Details Pages
+function displayBackgroundImage(type, backdropPath) {
+  const overlayDiv = document.createElement('div');
+  overlayDiv.style.backgroundImage = `url(https://image.tmdb.org/t/p/original/${backdropPath})`;
+
+  overlayDiv.style.backgroundSize = 'cover';
+  overlayDiv.style.backgroundPosition = 'center';
+  overlayDiv.style.backgroundRepeat = 'no-repeat';
+  overlayDiv.style.height = '100vh';
+  overlayDiv.style.width = '100vw';
+  overlayDiv.style.position = 'absolute';
+  overlayDiv.style.top = '0';
+  overlayDiv.style.left = '0';
+  overlayDiv.style.zIndex = '-1';
+  overlayDiv.style.opacity = '0.1';
+
+  if (type === 'movie') {
+    document.querySelector('#movie-details').appendChild(overlayDiv);
+  } else {
+    document.querySelector('#show-details').appendChild(overlayDiv);
+  }
+}
+
 // Fetch data from TMDB API
 async function fetchApiData(endpoint) {
   const API_KEY = '083d5692de280873c184435311901117';
@@ -170,7 +260,7 @@ function init() {
       displayMovieDetails();
       break;
     case '/tv-details.html':
-      console.log('tv details');
+      displayShowDetails();
       break;
     case 'search.html':
       console.log('search');
